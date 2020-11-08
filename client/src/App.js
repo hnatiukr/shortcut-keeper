206b0ff763e282d288b41531460fc2b291d00513
Roman <gnatyuk.info@gmail.com>
2020-11-09 00:08:40 +0200

    Initializing React Application

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'materialize-css';
+
+function App() {
+  return (
+    <div className='container'>
+      <h2>App</h2>
+    </div>
+  );
+}
+
+export default App;
