229ff8fda5cfe35b9f21ca94834d8f5ae0d18a1f
Roman <gnatyuk.info@gmail.com>
2020-11-09 14:29:57 +0200

    Add materialize style layout

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -2,8 +2,44 @@ import React from 'react';
 
 const AuthPage = () => {
   return (
-    <div>
-      <h1>Auth Page</h1>
+    <div className='row'>
+      <div className='col s6 offset-s3'>
+        <h1 className='yellow accent-4 black-text auth_h1'>Link Shortener</h1>
+        <div className='card'>
+          <div className='card-content white-text'>
+            <span className='card-title'>Authorization</span>
+            <div>
+              <div className='input-field'>
+                <input
+                  className='validate auth_input'
+                  id='email'
+                  type='email'
+                  name='email'
+                />
+                <label className='auth_label' htmlFor='email'>
+                  Email
+                </label>
+              </div>
+
+              <div className='input-field'>
+                <input
+                  id='password'
+                  type='password'
+                  name='password'
+                  className='validate auth_input'
+                />
+                <label className='auth_label' htmlFor='password'>
+                  Password
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className='card-action auth_btns'>
+            <button className='btn black white-text'>Sign up</button>
+            <button className='btn yellow accent-4 black-text'>Sign in</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
