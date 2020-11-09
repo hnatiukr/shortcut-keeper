694ab4e52e27119d6292fee0c3d0ff614ff68d38
Roman <gnatyuk.info@gmail.com>
2020-11-09 22:05:17 +0200

    Import and connect to context provider

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,29 @@ import React from 'react';
 import 'materialize-css';
 
 // services
+import { AuthContext } from './context/AuthContext';
 import { useRoutes } from './routes';
+import { useAuth } from './hooks/auth.hook';
 
 function App() {
-  const routes = useRoutes(false);
+  const { token, login, logout, userId } = useAuth();
+  const isAuthenticated = !!token;
 
-  return <div className='container'>{routes}</div>;
+  const routes = useRoutes(isAuthenticated);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token,
+        login,
+        logout,
+        userId,
+        isAuthenticated,
+      }}
+    >
+      <div className='container'>{routes}</div>
+    </AuthContext.Provider>
+  );
 }
 
 export default App;
