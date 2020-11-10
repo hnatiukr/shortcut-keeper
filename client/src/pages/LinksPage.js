f2528bcdc4a8a739ff40a3d9e0e70cd84f807835
Roman <gnatyuk.info@gmail.com>
2020-11-10 22:30:51 +0200

    Add state, hooks, context and content logic

--- client/src/pages/LinksPage.js
+++ client/src/pages/LinksPage.js
@@ -1,11 +1,38 @@
-import React from 'react';
+// libraries
+import React, { useCallback, useContext, useState, useEffect } from 'react';
+
+// services
+import { AuthContext } from '../context/AuthContext';
+import { useHttp } from '../hooks/http.hook';
+
+// components
+import Loader from '../components/Loader';
+import LinksList from '../components/LinksList';
 
 const LinksPage = () => {
-  return (
-    <div>
-      <h1>Links Page</h1>
-    </div>
-  );
+  const [links, setLinks] = useState([]);
+  const { loading, request } = useHttp();
+  const { token } = useContext(AuthContext);
+
+  const fetchLinks = useCallback(async () => {
+    try {
+      const fetched = await request('/api/link', 'GET', null, {
+        Authorization: `Bearer ${token}`,
+      });
+
+      setLinks(fetched);
+    } catch (error) {}
+  }, [token, request]);
+
+  useEffect(() => {
+    fetchLinks();
+  }, [fetchLinks]);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return !loading && <LinksList links={links} />;
 };
 
 export default LinksPage;
