b3afc692973055cb7af00a8f627506f2370e2800
Roman <gnatyuk.info@gmail.com>
2020-11-10 22:28:12 +0200

    Show loader if not ready

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,16 @@ import { useAuth } from './hooks/auth.hook';
 
 // components
 import Navbar from './components/Navbar';
+import Loader from './components/Loader';
 
 function App() {
-  const { token, login, logout, userId } = useAuth();
+  const { token, login, logout, userId, ready } = useAuth();
   const isAuthenticated = !!token;
-
   const routes = useRoutes(isAuthenticated);
+
+  if (!ready) {
+    return <Loader />;
+  }
 
   return (
     <AuthContext.Provider
